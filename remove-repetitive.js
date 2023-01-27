5d552b97c42857827c0a9125ebac1dd287080cfe
masoud-shojaei <msodshojaei@gmail.com>
2023-01-28 00:40:34 +0330

    Add usage to remove-repetitive.js

--- remove-repetitive.js
+++ remove-repetitive.js
@@ -1,3 +1,14 @@
+/*
+|--------------------------------------------------------------------------
+| Usage
+|--------------------------------------------------------------------------
+|
+| Invoke RemoveRepetitive method, then pass the string
+| ex:
+|   RemoveRepetitive("aaaaaaaaaaaaaaabbbbbbbbbbbbzzzzzzzzzzzz")
+*/
+
+
 //file-name: remove-repetitive.js
 function RemoveRepetitive(str) {
     let rawString = str
