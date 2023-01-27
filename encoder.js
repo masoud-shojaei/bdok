7605c3ea95a6347c2347063b983a698fd1c46367
masoud-shojaei <msodshojaei@gmail.com>
2023-01-28 00:19:48 +0330

    Implement encoding functionality

--- encoder.js
+++ encoder.js
@@ -1,22 +1,56 @@
-
 //file-name: encoder.js
-class  encoder{
+export default class encoder{
     constructor() {
+        this.alphabet = "abcdefghijklmnopqrstuvwxyz"
     }
 
-    getEncode(rawString, countOfEncode) {
+    getEncode(rawString, countOfEncode = 1) {
         const firstStepOfEncode = this.letterTransfer(rawString)
-        console.log("firstStepOfEncode", firstStepOfEncode)
+        let encodedString = null
+
+        for (let i = 1; i<=countOfEncode; i++) {
+            if (i==1) {
+                encodedString = this.encodeTheString(firstStepOfEncode)
+            } else {
+                encodedString = this.encodeTheString(encodedString)
+            }
+        }
+
+        return encodedString;
+    }
+
+    encodeTheString(stringToEncode) {
+        let encodedArr = []
+        let arrayOfLetters = this.convertStringToArray(stringToEncode)
+
+        arrayOfLetters.forEach((letter) => {
+            const CurrentLetterIndex = this.alphabet.indexOf(String(letter))
+            const nextLetterIndex = (CurrentLetterIndex == 25) ? 0 : CurrentLetterIndex + 1
+            const nextLetter = this.alphabet[nextLetterIndex]
+
+            encodedArr.push(nextLetter)
+        })
+
+        return this.convertArrayToString(encodedArr)
+    }
+
+    convertArrayToString(arr) {
+        const str = arr.toString().replaceAll(",","")
+
+        return str
+    }
+
+    convertStringToArray(string) {
+        const arr = [...string]
+
+        return arr
     }
 
     letterTransfer(stringForTransfer) {
-        let arrayOfLetters = [...stringForTransfer]
+        let arrayOfLetters = this.convertStringToArray(stringForTransfer)
         const lastItem = arrayOfLetters.pop()
 
         arrayOfLetters.unshift(lastItem)
-        return arrayOfLetters
+        return this.convertArrayToString(arrayOfLetters)
     }
 }
-
-const test = new encoder
-test.getEncode("abz")
