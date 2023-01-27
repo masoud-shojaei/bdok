e92078f53e996cf1c4f21d2feb5b8cf6ad0ad65e
masoud-shojaei <msodshojaei@gmail.com>
2023-01-27 10:41:33 +0330

    Implement init method

--- generalize.js
+++ generalize.js
@@ -1,4 +1,19 @@
 //file-name: generalize.js
-function generalize() {
+class generalize {
+    constructor() {
+        this.availableItems = ['{}','[]','()']
+        this.openCharacters = []
+        this.closeCharacters = []
+        this.init()
+    }
 
+    init() {
+        this.availableItems.forEach((item) => {
+            const openCharacter = item.slice(0,1)
+            const closeCharacter = item.slice(1,2)
+
+            this.openCharacters.push(openCharacter)
+            this.closeCharacters.push(closeCharacter)
+        })
+    }
 }
